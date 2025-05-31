932e01b200663c548ec4a4de0f5e20c3622f8717
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2025-05-31 15:37:03 -0500

    header y otras cosas

--- javas.js
+++ javas.js
@@ -1,10 +1,12 @@
 function bar() {
   const nav = document.getElementById("main-nav");
 
-  if (nav.style.display === "none" || nav.style.display === "") {
-    nav.style.display = "block";
-    anima;
-  } else {
-    nav.style.display = "none";
+  if (window.innerWidth <= 600) {
+    if (nav.style.display === "none" || nav.style.display === "") {
+      nav.style.display = "block";
+      anima;
+    } else {
+      nav.style.display = "none";
+    }
   }
 }
